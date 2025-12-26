872c5af6c635b251ad5aecd15a7a7c9fb489773d
Erick Velez <erickvelez7@gmail.com>
2025-12-26 11:04:08 -0800

    add some new plugins, add new keybinds

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,7 +1,15 @@
+local opts = { noremap = true, silent = true }
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep in dir' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'List Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'List help tags' })
 vim.keymap.set('n', '<leader>fs', builtin.current_buffer_fuzzy_find, { desc = 'Search buffer' })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Move down in buffer with cursor centered"})
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Move up in buffer with cursor centered"})
+
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
 
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
   'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
   dependencies = { 
     'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     "nvim-tree/nvim-web-devicons",
   }
 }
